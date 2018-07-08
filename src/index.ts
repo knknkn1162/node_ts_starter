9097a105725c0dd8e6c5283f7b93e6c36ff1b88d
knknkn <knknkn1162@gmail.com>
2018-07-08 22:57:49 +0900

    resolve http module

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
-class Student {
-    fullName: string;
-    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
-        this.fullName = firstName + " " + middleInitial + " " + lastName;
-    }
+import * as http from "http";
+
+class Main {
+    constructor() {
+        // httpサーバーを設定する
+        const server: http.Server = http.createServer(
+            (request: http.IncomingMessage, response: http.ServerResponse) =>
+                this.requestHandler(request, response));
+        // サーバーを起動してリクエストを待ち受け状態にする
+        server.listen("5000");
+  }
+  /*
+  * サーバーにリクエストがあった時に実行される関数
+  */
+  private requestHandler(request: http.IncomingMessage,
+                         response: http.ServerResponse): void {
+    response.end("Hello! Node.js with TypeScript");
+  }
 }
 
-interface Person {
-    firstName: string;
-    lastName: string;
-}
-
-function greeter(person : Person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-
-let user = new Student("Jane", "M.", "User");
-
-document.body.innerHTML = greeter(user);
+const main = new Main();
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
+var nodeExternals = require('webpack-node-externals');
 module.exports = {
  
     mode: 'development',
     // メインとなるJavaScriptファイル（エントリーポイント）
     entry: './src/index.ts',
-   
+    // See https://github.com/webpack/webpack/issues/603#issuecomment-185472374 or https://saku.io/build-for-node-runtime-using-webpack/.
+    // Otherwise, you can't resolve http with node_modules stream-http!
+    target: 'node',
+    externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+
     // ファイルの出力設定
     output: {
       //  出力ファイルのディレクトリ名
@@ -16,12 +21,14 @@ module.exports = {
             {
                 test: /\.ts$/,
                 use: 'ts-loader'
-            }
+            },
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
         ]
     },
 
     resolve: {
-        extensions: [".ts"]
+        // see the link, https://github.com/webpack/webpack-dev-server/issues/720#issuecomment-268470989
+        extensions: [".ts", ".js"],
     },
   };
   
