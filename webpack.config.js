7216f62cf0c28a3282805577fd8a491c8398e4fe
knknkn <knknkn1162@gmail.com>
2018-07-08 18:03:43 +0900

    fix package.json & webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ module.exports = {
  
     mode: 'development',
     // メインとなるJavaScriptファイル（エントリーポイント）
-    entry: `./src/index.ts`,
+    entry: './src/index.ts',
    
     // ファイルの出力設定
     output: {
       //  出力ファイルのディレクトリ名
       path: `${__dirname}/dist`,
       // 出力ファイル名
-      filename: 'main.js'
+      filename: 'bundle.js'
     },
     module: {
         rules: [
@@ -21,7 +21,7 @@ module.exports = {
     },
 
     resolve: {
-        extensions: [".ts", ".tsx", ".js", ".json"]
+        extensions: [".ts"]
     },
   };
   
